0d4d0cecde928558393793ac4bca5e8d7750bccc
fscene8 <im.afif@gmail.com>
2022-05-22 01:09:50 +0800

    completed of q3

--- dashboard.py
+++ dashboard.py
@@ -15,13 +15,12 @@ def chart():
         return render_template('ternd_chart.html')
     return redirect(url_for('auth.login'))
 
-    #dropdownlist 
-
 @dashboard.route('/trend_chart_per_user', methods=['GET'])
 @login_required
 def chart_per_user():
     user_list = []
     for user in User.objects: 
+        # Ignore ADMIN
         if not user.email == 'admin@abc.com':
             user_list.append(user)
     if current_user.email == 'admin@abc.com':
@@ -65,46 +64,52 @@ def generateIncomeInRange(data):
     for h in STAYCATION.objects:
         # Sort the bookings by date
         for book in Booking.objects(package=h.id).order_by('check_in_date'):
-            if toDate >= book.check_in_date >= fromDate: 
-                date_str = book.check_in_date.strftime("%Y-%m-%d")
-                if isViewAll:
-                    date_index = date_list.index(book.check_in_date)
-                    income_dict[h.hotel_name][date_index] += h.unit_cost   
-                else:
-                    if not h.hotel_name in hotel_label:
-                        hotel_label.append(h.hotel_name)
-                        income_label[h.hotel_name] = []
-                        income_dict[h.hotel_name] = []
-                    if date_str not in date_labels:
-                        date_labels.append(date_str)
-                    if date_str not in income_label[h.hotel_name]:
-                        income_label[h.hotel_name].append(date_str)
-                        income_dict[h.hotel_name].append(h.unit_cost)
+            if not book.customer.email == 'admin@abc.com': # Ignore ADMIN
+                if toDate >= book.check_in_date >= fromDate: 
+                    date_str = book.check_in_date.strftime("%Y-%m-%d")
+                    if isViewAll:
+                        date_index = date_list.index(book.check_in_date)
+                        income_dict[h.hotel_name][date_index] += h.unit_cost   
                     else:
-                        # add the income to existing date
-                        index = income_label[h.hotel_name].index(date_str)
-                        income_dict[h.hotel_name][index] += h.unit_cost
+                        if not h.hotel_name in hotel_label:
+                            hotel_label.append(h.hotel_name)
+                            income_label[h.hotel_name] = []
+                            income_dict[h.hotel_name] = []
+                        if date_str not in date_labels:
+                            date_labels.append(date_str)
+                        if date_str not in income_label[h.hotel_name]:
+                            income_label[h.hotel_name].append(date_str)
+                            income_dict[h.hotel_name].append(h.unit_cost)
+                        else:
+                            # add the income to existing date
+                            index = income_label[h.hotel_name].index(date_str)
+                            income_dict[h.hotel_name][index] += h.unit_cost
     date_labels.sort()
     return hotel_label, date_labels, income_dict, income_label, gap
 
 def generatePerChartData(data, charttype):
     x_label = [] # x-axis
     y_content = [] # bar height
-    if charttype == 'trend_chart_per_user':
+
+    if charttype == 'trend_chart_per_user': # Due Per User
+        # Get the user object form the MongoDB
         user_obj = User.objects(email=data['email']).first()
+
+        # Iterate the STAYCATION object
         for hotel in STAYCATION.objects:
-            # Capture the amount of bookings 
+            # Capture the bookings due per user by User object
             x_label.append(hotel.hotel_name)
             y_content.append(Booking.objects(package=hotel,customer=user_obj).count())
-    else:
+    else: # Due Per Hotel
+        # Get the STAYCATION object form the MongoDB
         hotel_obj = STAYCATION.objects(hotel_name=data['hotel']).first()
         for user in User.objects:
+            # Ignore Admin
             if not user.email == 'admin@abc.com':
-                # Capture the amount of bookings 
+                # Capture the bookings due per hotel by STAYCATION object
                 x_label.append(user.name)
                 y_content.append(Booking.objects(package=hotel_obj,customer=user).count())
     return x_label, y_content
-
 
 @dashboard.route('/get_chart_data', methods=['GET', 'POST'])
 @login_required    
@@ -116,6 +121,7 @@ def get_chart_data():
     # Generate the required variables for chart js
     data = request.get_json()
     charttype = data['charttype']
+    # If the AJAX calls from the total_income page
     if charttype == 'trend_chart':
         hotel_label, date_labels, income_dict, income_label, gap = generateIncomeInRange(data)
         return jsonify({'status' : 'OK',
@@ -126,6 +132,7 @@ def get_chart_data():
                         'income_dict' : income_dict,
                         'income_label': income_label,
                         'daysrange' : gap.days+1})
+    # If the AJAX calls from the Due Per User or Due Per Hotel page
     elif charttype == 'trend_chart_per_user' or charttype == 'trend_chart_per_hotel':
         x_label, y_content = generatePerChartData(data, charttype)
         return jsonify({'status' : 'OK',
